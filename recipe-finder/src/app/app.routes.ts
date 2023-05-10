14a9650d05e6ae83c3ebc6b350e850add9af635b
Francisco Arellano <SilverD.amld@gmail.com>
2023-05-10 12:44:53 +0000

    creating and setting project

---
+++ recipe-finder/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'second-page',
+    loadComponent: () => import('./second-page/second-page.page').then( m => m.SecondPagePage)
+  },
+];
